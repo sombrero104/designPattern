8940212ccc36b16e404b30549ee117235c2ad10a
sombrero104 <sombrero.khy@gmail.com>
2019-09-15 09:03:15 +0900

    .

---
+++ src/main/java/chain/chain2/LoggerTest.java
@@ -0,0 +1,20 @@
+package chain.chain2;
+
+public class LoggerTest {
+
+    public static void main(String[] args) {
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        System.out.println("-------------------------------------------------------");
+        errorLogger.logMessage(AbstractLogger.INFO, "This is an info level information.");
+        errorLogger.logMessage(AbstractLogger.DEBUG, "This is a debug level information.");
+        errorLogger.logMessage(AbstractLogger.ERROR, "This is an error level information.");
+        System.out.println("-------------------------------------------------------");
+    }
+
+}
