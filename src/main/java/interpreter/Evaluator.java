ac94852ca8f8d0177cd5be9887a511412c03b959
sombrero104 <sombrero.khy@gmail.com>
2019-09-17 19:56:19 +0900

    .

--- src/main/java/interpreter/Evaluator.java
+++ src/main/java/interpreter/Evaluator.java
@@ -6,6 +6,31 @@ import java.util.Stack;
 public class Evaluator implements Expression {
     private Expression syntaxTree;
 
+    /**
+     * 1. expression으로 Evaluator 객체 생성.
+     *
+     * a b c - +
+     * 위와 같은 expression이 들어왔을 때, 스택의 변화 순서.
+     *
+     * (1) a, b, c가 차례대로 스택에 쌓임.
+     * --------------------------------------------------
+     * Variable(c)
+     * Variable(b)
+     * Variable(a)
+     * --------------------------------------------------
+     *
+     * (2) b와 c가 pop되어 Minus로 쌓임.
+     * --------------------------------------------------
+     * Minus(Variable(b), Variable(c))
+     * Variable(a)
+     * --------------------------------------------------
+     *
+     * (3) 최종적으로 syntaxTree에 저장되는 형태.
+     * --------------------------------------------------
+     * Plus(Minus(Variable(b), Variable(c)), Variable(a))
+     * --------------------------------------------------
+     *
+     */
     public Evaluator(String expression) {
         Stack<Expression> expressionStack = new Stack<>();
         for(String token : expression.split(" ")) {
@@ -24,6 +49,23 @@ public class Evaluator implements Expression {
         syntaxTree = expressionStack.pop();
     }
 
+    /**
+     * 2. 실제 값을 넣고 계산.
+     *
+     * ----------------
+     * "a", Number(5)
+     * "b", Number(10)
+     * "c", Number(42)
+     * ----------------
+     * 위와 같은 variables 맵이 들어왔을 때,
+     *
+     * (1) syntaxTree에 저장된 아래 형태에서
+     *     겉에서부터 차례대로 variables값을 넘기면서 interpret()을 호출한다.
+     * --------------------------------------------------
+     * Plus(Minus(Variable(b), Variable(c)), Variable(a))
+     * --------------------------------------------------
+     *
+     */
     @Override
     public int interpret(Map<String, Expression> variables) {
         return syntaxTree.interpret(variables);
--- src/main/java/interpreter/InterpreterTest.java
+++ src/main/java/interpreter/InterpreterTest.java
@@ -6,13 +6,23 @@ import java.util.Map;
 public class InterpreterTest {
 
     public static void main(String[] args) {
+        /**
+         * 1. expression으로 Evaluator 객체 생성.
+         *    -> Evaluator.java의 Evaluator() 생성자 설명 참조.
+         */
         String expression = "a b c - +";
         Evaluator evaluator = new Evaluator(expression);
+
+        /**
+         * 2. 실제 값을 넣고 계산.
+         *    -> Evaluator.java의 interpret() 메소드 설명 참조.
+         */
         Map<String, Expression> variables = new HashMap<>();
         variables.put("a", new Number(5));
         variables.put("b", new Number(10));
         variables.put("c", new Number(42));
         int result = evaluator.interpret(variables);
+
         System.out.println("----------------------------------");
         System.out.println(expression);
         System.out.println("----------------------------------");
