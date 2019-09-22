8fd262f6a3a46efe13a59ccb68f547cd7aa947e2
sombrero104 <sombrero.khy@gmail.com>
2019-09-22 20:53:40 +0900

    .

---
+++ src/main/java/visitor/CarElementVisitor.java
@@ -0,0 +1,13 @@
+package visitor;
+
+public interface CarElementVisitor {
+
+    void visit(Wheel wheel);
+
+    void visit(Engine engine);
+
+    void visit(Body body);
+
+    void visit(Car car);
+
+}
